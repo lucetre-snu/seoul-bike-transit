0ca6dfea373b070bdef8c289ef95a378fd6631d7
lucetre <lucetre@naver.com>
2022-05-26 19:34:01 +0900

    Warning

--- route-finder/demo.py
+++ route-finder/demo.py
@@ -1,7 +1,10 @@
 import pickle
+
+from cv2 import mean
 import navigator
 import coordinate
 import pandas as pd
+import numpy as np
 from IPython.display import HTML
 
 def read_routes(org, des):
@@ -19,35 +22,47 @@ def read_routes(org, des):
 
     rows = []
     for i, srt in enumerate(bikes):
-        (_, (srt_name, srt_lat, srt_lng, _, _, srt_coord)) = srt
+        (_, (srt_name, _, _, _, _, srt_coord)) = srt
         for j in range(i+1, len(bikes)):
             try:
-                (_, (end_name, end_lat, end_lng, _, _, end_coord)) = bikes[j]
+                (_, (end_name, _, _, _, _, end_coord)) = bikes[j]
                 route_id = f'{i}-{j}'
+
+                try:    srt_calories = int(np.mean([float(route['info'].replace("kcal소모", "")) for route in org_routes[i]]))
+                except: srt_calories = 0
+                try:    end_calories = int(np.mean([float(route['info'].replace("kcal소모", "")) for route in des_routes[j]]))
+                except: end_calories = 0
+                try:    bike_calories = int(np.mean([float(route['calories'].replace("kcal", "")) for route in bike_routes[route_id]]))
+                except: end_calories = 0
+                total_calories = srt_calories + bike_calories + end_calories
+
                 srt_time = navigator.calc_timedelta(min([route['time'] for route in org_routes[i]]))
-                bike_time = navigator.calc_timedelta(min([route['time'] for route in bike_routes[route_id]]))
                 end_time = navigator.calc_timedelta(min([route['time'] for route in des_routes[j]]))
+                bike_time = navigator.calc_timedelta(min([route['time'] for route in bike_routes[route_id]]))
                 total_time = srt_time + bike_time + end_time
 
-                link1 = f'https://map.kakao.com/?map_type=TYPE_MAP&target=transit&rt={org_x},{org_y},{srt_coord[0]},{srt_coord[1]}&rt1={org}&rt2={srt_name}'
-                link2 = f'https://map.kakao.com/?map_type=TYPE_MAP&target=bike&rt={srt_coord[0]},{srt_coord[1]},{end_coord[0]},{end_coord[1]}&rt1={srt_name}&rt2={end_name}'
-                link3 = f'https://map.kakao.com/?map_type=TYPE_MAP&target=transit&rt={end_coord[0]},{end_coord[1]},{des_x},{des_y}&rt1={end_name}&rt2={des}'
-                link4 = f'https://map.kakao.com/?map_type=TYPE_MAP&target=transit&rt={org_x},{org_y},{des_x},{des_y}&rt1={org}&rt2={des}'
+                srt_link = f'https://map.kakao.com/?map_type=TYPE_MAP&target=transit&rt={org_x},{org_y},{srt_coord[0]},{srt_coord[1]}&rt1={org}&rt2={srt_name}'
+                end_link = f'https://map.kakao.com/?map_type=TYPE_MAP&target=transit&rt={end_coord[0]},{end_coord[1]},{des_x},{des_y}&rt1={end_name}&rt2={des}'
+                bike_link = f'https://map.kakao.com/?map_type=TYPE_MAP&target=bike&rt={srt_coord[0]},{srt_coord[1]},{end_coord[0]},{end_coord[1]}&rt1={srt_name}&rt2={end_name}'
+                total_link = f'https://map.kakao.com/?map_type=TYPE_MAP&target=transit&rt={org_x},{org_y},{des_x},{des_y}&rt1={org}&rt2={des}'
 
                 rows.append([ \
                     route_id, \
-                    org, \
-                    f'<a href="{link1}">{srt_time}</a>', \
-                    srt_name, \
-                    f'<a href="{link2}">{bike_time}</a>', \
-                    end_name, \
-                    f'<a href="{link3}">{end_time}</a>', \
-                    des, \
-                    f'<a href="{link4}">{total_time}</a>', \
+                    f'<a href="{srt_link}">{org}</a>', \
+                    str(srt_time), srt_calories, \
+                    f'<a href="{bike_link}">{srt_name}</a>', \
+                    str(bike_time), bike_calories, \
+                    f'<a href="{end_link}">{end_name}</a>', \
+                    str(end_time), end_calories, \
+                    f'<a href="{total_link}">{des}</a>', \
+                    str(total_time), total_calories, \
                 ])
             except:
                 pass
 
-    df = pd.DataFrame(rows, columns=["route_id", "org", "srt_time", "srt_name", "bike_time", "end_name", "end_time", "des", "total_time"])
+    df = pd.DataFrame(rows, columns="route_id org srt_time srt_calories srt_name bike_time bike_calories end_name end_time end_calories des total_time total_calories".split())
     df.set_index("route_id", inplace=True)
     return HTML(df.sort_values(by="total_time").to_html(escape=False))
+
+if __name__ == "__main__":
+    read_routes('낙성대공원', '몰로코')
